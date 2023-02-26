d5b2c8980a057cda21f92861aed9547a31fb2813
BRAHIM MAHIOUSSI <37108275+BRAHAIM360@users.noreply.github.com>
2023-02-26 16:48:22 +0100

    add Item Card component

--- client/src/components/ItemCard/ItemCard.tsx
+++ client/src/components/ItemCard/ItemCard.tsx
@@ -1,23 +1,50 @@
 import React from 'react'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button';
+import { CardActionArea } from '@mui/material';
+import Container from '@mui/material/Container';
 import "./style.scss"
+import { useNavigate } from 'react-router-dom';
 
-export interface ItemProps {
+export interface ItemCardProps {
     id: number;
     name: string;
     description: string;
     image: string;
     endOfAuction: Date;
     currentPrice: number;
+    buttonAction?: () => void;
 }
 
-export const ItemCard = ({ id, name, description, currentPrice, endOfAuction, image }: ItemProps) => {
+export const ItemCard = ({ id, name, description, currentPrice, endOfAuction, image, buttonAction }: ItemCardProps) => {
+    const navigate = useNavigate();
     return (
-        <div className="item-card">
-            <img className='item-card__image' src={image} alt="Denim Jeans" />
-            <h1 className='item-card__title'>{name}</h1>
-            <p className="item-card__price">${currentPrice}</p>
-            <p className='item-card__description'>{description}</p>
-            <button className='item-card__button' onClick={() => { }}>Add to Cart</button>
-        </div>
+        <Card sx={{ maxWidth: 300, cursor: "default" }}>
+            <CardMedia
+                component="img"
+                height="140"
+                image={image}
+                alt="green iguana"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary" sx={{
+                    display: '-webkit-box',
+                    overflow: 'hidden',
+                    WebkitBoxOrient: 'vertical',
+                    WebkitLineClamp: 4,
+                }}>
+                    {description}
+                </Typography>
+                <Container maxWidth="sm" sx={{ display: "flex", justifyContent: "center", marginTop: '1rem' }}>
+                    <Button variant="contained" onClick={() => navigate(`items/${id}`)}>Bid Now</Button>
+                </Container>
+            </CardContent>
+        </Card>
     )
 }
