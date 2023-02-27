5423cd51613aa3b1b763712e784f21ec9b6c8fc0
BRAHIM MAHIOUSSI <37108275+BRAHAIM360@users.noreply.github.com>
2023-02-28 00:45:44 +0100

    updated the app controler with swager tags

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from "@nestjs/common";
-import { ApiBadRequestResponse, ApiOkResponse } from "@nestjs/swagger";
+import { ApiBadRequestResponse, ApiOkResponse, ApiTags } from "@nestjs/swagger";
 import { AppService } from "./app.service";
-
+@ApiTags("root")
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,3 +1,5 @@
+import { BidModule } from "./bid/bid.module";
+import { UserModule } from "./user/user.module";
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
@@ -9,7 +11,8 @@ import { ItemModule } from "./item/item.module";
 import { AuthModule } from "./auth/auth.module";
 import { APP_GUARD } from "@nestjs/core";
 import { AtGuard } from "./common/guard";
-
+import * as express from "express";
+import { UploadModule } from "./upload/upload.module";
 @Module({
   imports: [
     JwtModule.register({}),
@@ -18,8 +21,22 @@ import { AtGuard } from "./common/guard";
     PrismaModule,
     ItemModule,
     AuthModule,
+    UploadModule,
+    UserModule,
+    BidModule,
   ],
   controllers: [AppController],
   providers: [AppService, { provide: APP_GUARD, useClass: AtGuard }],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: any) {
+    // Create an instance of the express application
+    const app = express();
+
+    // Serve files in the public folder
+    app.use(express.static("uploads"));
+
+    // Mount the express application
+    consumer.apply(app).forRoutes("*");
+  }
+}
