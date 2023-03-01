257402b05886659ef7162a2b9f237a78f37a4445
BRAHIM MAHIOUSSI <37108275+BRAHAIM360@users.noreply.github.com>
2023-03-02 00:32:03 +0100

    add theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,34 +2,53 @@ import React from "react"
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { AdminPage, Home, ItemDetails, Login } from "./pages";
 import { RootState, useAppSelector } from "./store";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import brown from "@mui/material/colors/brown";
+import green from "@mui/material/colors/green";
+import { CssBaseline } from "@mui/material";
 
 
 function App() {
-  const { isLogged, isAdmin } = useAppSelector((state: RootState) => state.auth)
+  const { isLogged, isAdmin, darkTheme } = useAppSelector((state: RootState) => state.auth)
 
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: brown[500],
+
+      },
+      secondary: {
+        main: green[300],
+      },
+      mode: darkTheme ? "dark" : "light",
+    },
+  });
   return (
 
     <React.Suspense fallback={<div>Loading...</div>}>
-      {isLogged
-        ? <Routes>
-          <Route path="/items/:id" element={<ItemDetails />} />
-          {isAdmin && <Route path="/admin" element={<AdminPage />} />}
-          <Route path="/" element={<Home />} />
-          <Route
-            path="*"
-            element={<Navigate to="/" replace />}
-          />
-        </Routes>
-        : <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="*"
-            element={<Navigate to="/login" replace />}
-          />
-        </Routes>
-      }
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {isLogged
+          ? <Routes>
+            <Route path="/items/:id" element={<ItemDetails />} />
+            {isAdmin && <Route path="/admin" element={<AdminPage />} />}
+            <Route path="/" element={<Home />} />
+            <Route
+              path="*"
+              element={<Navigate to="/" replace />}
+            />
+          </Routes>
+          : <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="*"
+              element={<Navigate to="/login" replace />}
+            />
+          </Routes>
+        }
 
-
+      </ThemeProvider>
     </React.Suspense>
   )
 }
